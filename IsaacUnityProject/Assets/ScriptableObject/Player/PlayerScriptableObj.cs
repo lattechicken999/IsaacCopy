aa60cb3b5bed5023ad91f4b9223650e09717b80d
lattechicken999 <whdtn218@gmail.com>
2025-11-22 01:13:43 +0900

    [Resource][Feat] Bullet 용 애니메이션 제작/Player용 Scriptable Object 생성

---
+++ IsaacUnityProject/Assets/ScriptableObject/Player/PlayerScriptableObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="PlayerSO",menuName ="ScripableObject/PlayerSO")]
+public class PlayerScriptableObj : ScriptableObject
+{
+    [Header("Status")]
+    public float Damage = 3.7f;
+    public float Tears =2.73f;
+    public float Range = 20f;
+    public float ShotSpeed = 0.8f;
+    public float Speed = 1.4f;
+    public int Heart = 3;
+    public int SoulHeart = 0;
+
+    [Header("Prefebs")]
+    public GameObject TearsPrefeb;
+}
