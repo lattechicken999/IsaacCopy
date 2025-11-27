099eefc6c2bad80f57fa5f64216df8ab9a1c8f14
lattechicken999 <whdtn218@gmail.com>
2025-11-27 10:39:35 +0900

    [Feat] 피격시 뒤로 밀려나게 수정

--- IsaacUnityProject/Assets/Scripts/Player/PlayerBattle.cs
+++ IsaacUnityProject/Assets/Scripts/Player/PlayerBattle.cs
@@ -8,8 +8,12 @@ public class PlayerBattle : MonoBehaviour
 
     private float _invincibility =0;
     private bool _attacked;
+    private Rigidbody2D _parentRig;
 
-    
+    private void Start()
+    {
+        _parentRig = transform.parent.gameObject.GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         if (_attacked)
@@ -25,7 +29,10 @@ public class PlayerBattle : MonoBehaviour
         if (!_attacked && collision.gameObject.CompareTag("Monster"))
         {
             _attacked = true;
+            //피격시에는 움직임 정지
+            _pm.SetPlayerMoveValue(Vector2.zero);
             _pm.TakeDamage(collision.gameObject.GetComponent<MonsterModel>().AttackPoint);
+            _parentRig.AddForce((transform.position - collision.transform.position).normalized * 10 , ForceMode2D.Impulse);
         }
     }
 
